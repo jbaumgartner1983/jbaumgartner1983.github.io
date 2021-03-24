3438fa8c742474c3e2f1d51fcc94979cb35ec9dc
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-24 22:22:57 +0100

    String Interpolation

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,2 +1,3 @@
-let favoriteAnimal = 'dog';
-console.log('My favorite animal: ' + favoriteAnimal);
+const myName = 'Ivan';
+const myCity = 'Bregenz';
+console.log (`My name is ${myName}. My favorite city is ${myCity}.`)
