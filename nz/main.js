548a2a4906728aa5fbec7d34eb7687cbe20ee5a9
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-25 16:00:59 +0100

    Koordinaten

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,6 @@
-const map = L.map ("map");
+const map = L.map ("map", {  //objekt erstellt, liste einfürgen []
+    center: [64.3137, -20.2995] //liste mit koordinaten, lat und long koordinaten
+});
+//64.3137, -20.2995
 console.log(document.querySelector("#map"));
+
