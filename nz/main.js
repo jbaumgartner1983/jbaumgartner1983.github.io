27aceab6d8a5bb7cb9a6f74e78e94afc912c57a0
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-25 17:15:32 +0100

    stopp ersetzten mit entry

--- nz/main.js
+++ nz/main.js
@@ -21,16 +21,16 @@ const map = L.map ("map", {  //objekt erstellt, liste einfürgen []
 });
 
 console.log(ROUTE)
-for (let entry of ROUTE) {
+for (let entry of ROUTE) {       //for Schleife erstellen
     console.log(entry);
-}
+    let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+    mrk.bindPopup(`
+      <h4>Stop ${entry.nr}: ${entry.name} </h4>
+      <p><i class= "fas fa-external-link-alt mr-3"></i><a href = "${entry.wikipedia}">Read about stop in Wikipedia</a>
+      </p>
+    `).openPopup();
 
-let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
-mrk.bindPopup(`
-<h4>Stop ${stop.nr}: ${stop.name} </h4>
-<p><i class= "fas fa-external-link-alt mr-3"></i><a href = "${stop.wikipedia}">Read about stop in Wikipedia</a>
-</p>
-`).openPopup();
+}
 
 
 //64.3137, -20.2995
