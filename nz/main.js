8ad6c6dd17f9c213a1a73c80d244426877b0b65a
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-25 16:28:02 +0100

    let stop und console log einfügen

--- nz/main.js
+++ nz/main.js
@@ -1,3 +1,17 @@
+let stop = {
+    nr: 1,
+    name: "Geysire",
+    lat: 64.3137,
+    lng: -20.2995,
+    user: "jbaumgartner1983"
+    wikipedia: "https://en.wikipedia.org/wiki/Geysir"
+}
+console.log(stop);
+console.log(stop.name);
+console.log(stop.lat);
+console.log(stop.lng);
+console.log(stop.wikipedia);
+
 const map = L.map ("map", {  //objekt erstellt, liste einfürgen []
     center: [64.3137, -20.2995], //liste mit koordinaten, lat und long koordinaten
     zoom: 13 //1 ist die ganze Welt
