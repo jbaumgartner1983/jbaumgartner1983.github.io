9d4d32d6732ae0304dbc90ea0e5325300392af6a
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-25 16:08:14 +0100

    map link openstreatmap

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,9 @@
 const map = L.map ("map", {  //objekt erstellt, liste einfürgen []
-    center: [64.3137, -20.2995] //liste mit koordinaten, lat und long koordinaten
+    center: [64.3137, -20.2995], //liste mit koordinaten, lat und long koordinaten
+    zoom: 13 //1 ist die ganze Welt
+    layers: [              // ist ein array
+        L.titleLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")  //server {s} von open street map, {z} ist der zoomlayer
+    ]  
 });
 //64.3137, -20.2995
 console.log(document.querySelector("#map"));
