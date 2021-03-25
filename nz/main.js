2797ed8e517921100aff3ab94fb8ff2bd5242121
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-03-25 16:22:04 +0100

    fehler korrigiert

--- nz/main.js
+++ nz/main.js
@@ -2,7 +2,7 @@ const map = L.map ("map", {  //objekt erstellt, liste einfürgen []
     center: [64.3137, -20.2995], //liste mit koordinaten, lat und long koordinaten
     zoom: 13 //1 ist die ganze Welt
     layers: [              // ist ein array
-        L.titleLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")  //server {s} von open street map, {z} ist der zoomlayer
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")  //server {s} von open street map, {z} ist der zoomlayer
     ]  
 });
 
