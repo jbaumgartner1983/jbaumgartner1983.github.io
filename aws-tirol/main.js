88a4258b50afed25f1dee4c15a19211833da1242
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-04-15 16:03:19 +0200

    main.js bsamap einfügen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,1 +1,18 @@
+let basemapGray = L.tileLayer.provider (" BasemapAT.basemap"),
 
+let map = L.map ("map", {
+    center: [47, 11],
+    zoom: 9,
+    layers: [
+        basemapGray
+    ]
+    });
+
+
+    let layerControl = L.control.layers({
+        "BasemapAT.basemap": basemapGray,
+        "BasemapAT.orthofoto": L.tileLayer.provider ("BasemapAT.basemap")
+    }).addTo(map);
+
+
+ 
