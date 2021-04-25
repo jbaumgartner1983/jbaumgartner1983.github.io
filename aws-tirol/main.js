bd0753c0d530420688fea9228b551f8904ce3dc2
jbaumgartner1983 <julia.baumgartner@student.uibk.ac.at>
2021-04-25 20:32:12 +0200

    let overlays einfügen, let awsLayer auskommentiert

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -8,6 +8,16 @@ let map = L.map("map", {
         basemapGray
     ]
 });
+
+let overlays = {
+    stations: L.featureGroup(),
+    temperature: L.featureGroup(),
+    snowheight: L.featureGroup(),
+    windspeed: L.featureGroup(),
+    winddirection: L.featureGroup()
+};
+
+
 //https://leafletjs.com/reference-1.7.1.html#control
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
@@ -18,11 +28,47 @@ let layerControl = L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+
+}, {
+    "Wetterstationen Tirol": overlays.stations,
+    "Temperatur (°C)": overlays.temperature,
+    "Schneehöhe (cm)": overlays.snowheight,
+    "Windgeschwindigkeit (km/h)":  overlays.windspeed,
+    "Windrichtung": overlays.winddirection
+}, {
+    collapsed: false
+
+
 }).addTo(map);
+
+let getColor = (value, colorRamp) => {
+    //console.log("Wert:", value, "Palette:", colorRamp);
+    for (let rule of colorRamp) {
+        if (value >= rule.min && value < rule.max) {
+            return rule.col;
+        }
+    }
+    return "black";
+};
+
+let newLabel = (coords, options) => {
+    let color = getColor(options.value, options.colors);
+    //console.log("Wert", options.value, "bekommt Farbe", color);
+    let label = L.divIcon({
+        html: `<div style="background-color:${color}">${options.value}</div>`,
+        className: "text-label"
+    })
+    let marker = L.marker([coords[1], coords[0]], {
+        icon: label,
+        title: `${options.station} (${coords[2]}m)`
+    });
+    return marker;
+};
+
 
 let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson";
 
-//https://leafletjs.com/reference-1.7.1.html#featuregroup
+/*https://leafletjs.com/reference-1.7.1.html#featuregroup
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
 // awsLayer.addTo(map);
@@ -34,7 +80,7 @@ layerControl.addOverlay(windLayer, "Windgeschwindigkeit (km/h)");
 windLayer.addTo(map);
 let temperatureLayer = L.featureGroup();
 layerControl.addOverlay(temperatureLayer, "Celsius (C)");
-temperatureLayer.addTo(map);
+temperatureLayer.addTo(map);*/
 
 
 //https://leafletjs.com/reference-1.7.1.html#marker
@@ -59,7 +105,7 @@ fetch(awsUrl)
                 <li>Temperatur: ${station.properties.LT} C</li>
                 <li>Schneehöhe: ${station.properties.HS || "?"} cm</li>
                 <li>Windgeschwindigkeit: ${station.properties.WG || "?"} km/h</li>
-                <li>Windgeschwindrichtung: ${station.properties.WR || "?"}</li>
+                <li>Windrichtung: ${station.properties.WR || "?"}</li>
                 </ul>
                 <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
                 `);
